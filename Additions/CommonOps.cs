9eea373a97fdef3ba98876b76b5ad83f9d18e1c5
Howard Good <v-hogood@outlook.com>
2021-01-10 10:02:15 -0800

    Add files via upload

---
+++ Additions/CommonOps.cs
@@ -0,0 +1,13 @@
+﻿namespace Org.Tensorflow.Lite.Support.Common.Ops
+{
+    public partial class CastOp
+    {
+        public global::Java.Lang.Object Apply(global::Java.Lang.Object p0)
+        { return Apply(p0 as global::Org.Tensorflow.Lite.Support.Tensorbuffer.TensorBuffer); }
+    }
+    public partial class NormalizeOp
+    {
+        public global::Java.Lang.Object Apply(global::Java.Lang.Object p0)
+        { return Apply(p0 as global::Org.Tensorflow.Lite.Support.Tensorbuffer.TensorBuffer); }
+    }
+}
---
+++ Additions/ImageOps.cs
@@ -0,0 +1,23 @@
+﻿namespace Org.Tensorflow.Lite.Support.Image.Ops
+{
+    public partial class ResizeOp
+    {
+        public global::Java.Lang.Object Apply(global::Java.Lang.Object p0)
+        { return Apply(p0 as global::Org.Tensorflow.Lite.Support.Image.TensorImage); }
+    }
+    public partial class ResizeWithCropOrPadOp
+    {
+        public global::Java.Lang.Object Apply(global::Java.Lang.Object p0)
+        { return Apply(p0 as global::Org.Tensorflow.Lite.Support.Image.TensorImage); }
+    }
+    public partial class Rot90Op
+    {
+        public global::Java.Lang.Object Apply(global::Java.Lang.Object p0)
+        { return Apply(p0 as global::Org.Tensorflow.Lite.Support.Image.TensorImage); }
+    }
+    public partial class TensorOperatorWrapper
+    {
+        public global::Java.Lang.Object Apply(global::Java.Lang.Object p0)
+        { return Apply(p0 as global::Org.Tensorflow.Lite.Support.Image.TensorImage); }
+    }
+}
