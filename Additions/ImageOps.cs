36eda842f3462a78caf4ac83e10d3232c03cddf0
Howard Good <v-hogood@outlook.com>
2021-05-18 10:15:51 -0700

    tensorflow-lite-supprt release 0.2.0

--- Additions/ImageOps.cs
+++ Additions/ImageOps.cs
@@ -1,5 +1,10 @@
 ﻿namespace Org.Tensorflow.Lite.Support.Image.Ops
 {
+    public partial class TransformToGrayscaleOp
+    {
+        public global::Java.Lang.Object Apply(global::Java.Lang.Object p0)
+        { return Apply(p0 as global::Org.Tensorflow.Lite.Support.Image.TensorImage); }
+    }
     public partial class ResizeOp
     {
         public global::Java.Lang.Object Apply(global::Java.Lang.Object p0)
