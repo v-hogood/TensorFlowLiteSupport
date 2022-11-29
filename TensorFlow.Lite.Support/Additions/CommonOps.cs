6560a86ba3aee045d0f38042c0605d179f7b19bc
Howard Good <v-hogood@outlook.com>
2022-11-28 16:09:25 -0800

    Remove Xamarin.* prefix from TensorFlowLiteSupport packages.

---
+++ TensorFlow.Lite.Support/Additions/CommonOps.cs
@@ -0,0 +1,13 @@
+﻿namespace TensorFlow.Lite.Support.Common.Ops
+{
+    public partial class CastOp
+    {
+        public global::Java.Lang.Object Apply(global::Java.Lang.Object p0)
+        { return Apply(p0 as global::TensorFlow.Lite.Support.TensorBuffer.TensorBuffer); }
+    }
+    public partial class NormalizeOp
+    {
+        public global::Java.Lang.Object Apply(global::Java.Lang.Object p0)
+        { return Apply(p0 as global::TensorFlow.Lite.Support.TensorBuffer.TensorBuffer); }
+    }
+}
